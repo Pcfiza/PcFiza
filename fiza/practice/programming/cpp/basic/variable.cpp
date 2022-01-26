7f3ff85ab59501420ec936e228e719bcaaabcead
Fiza Sheikh <98210047+Pcfiza@users.noreply.github.com>
2022-01-27 00:01:50 +0530

    Create variable.cpp

---
+++ fiza/practice/programming/cpp/basic/variable.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+
+using namespace std
+
+int main()
+{
+  string myName = "Fiza Sheikh" // string
+    
+  unsigned short int myAge = 18; // short number
+  
+  signed short int temprature = -45; // negative value
+    
+  float myHeight = 6.4; // floating point value
+  
+  bool isFizaMarried = false; // fiza is not married
+    
+  bool isFizaSingle = true; // fiza is single
+  
+  char firstLetterOfMyName = 'f'; // character value
+  
+  string myFavoriteCities[3] = {"Mumbai","Ahmedabad","Surat"};
+  
+  // you can see different data type's variable declaration 
+    
+  return 0;
+  
+}
