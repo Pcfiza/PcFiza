35b763a705101cacf2a9cd3065ddd72c311ba486
Fiza Sheikh <98210047+Pcfiza@users.noreply.github.com>
2022-01-26 23:57:36 +0530

    Create hello_world.cpp

---
+++ fiza/practice/programming/cpp/basic/hello_world.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+ 
+  cout<<"Hello World!" << endl;
+  
+  return 0;
+  
+  // this program displays Hello World! into Console through using console out Object and the insertation operator insert quoted Text into console
+  
+  
+  
+}
